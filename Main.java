dc8c143033e3a62751d1051cce082dc1cdd472db
manish batgiri <manishbatgiri9912@gmail.com>
2026-04-09 12:26:17 +0530

    updated class

--- Main.java
+++ Main.java
@@ -2,9 +2,9 @@ public class Main {
     int x = 5;
 
     public static void main(String[] args) {
-        Car myCar = new Car("Volvo", 9000000, "Graphite Grey");
-        System.out.println(myCar.name + " " + myCar.price + " " + myCar.color);
-        myCar.speed(200);
-        myCar.fullThrottle();
+       Person myName = new Person();
+       System.out.println(myName.getName());
+       myName.setName("Radha");
+       System.out.println(myName.getName());
     }
 }
