8537ce6ff9d4aab4cba2e9dce370d5486f7ecd50
manish batgiri <manishbatgiri9912@gmail.com>
2026-06-05 23:03:50 +0530

    created doubly linked list class

---
+++ src/doublyLinkedList/DoublyLinkedList.java
@@ -0,0 +1,34 @@
+package doublyLinkedList;
+
+public class DoublyLinkedList {
+    private Node head;
+    private Node tail;
+    private int length;
+
+    static class Node {
+        int value;
+        Node next;
+        Node prev;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public DoublyLinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public void getHead() {
+        System.out.println("Head: " + head.value);
+    }
+    public void getTail() {
+        System.out.println("Tail: " + tail.value);
+    }
+    public void getLength() {
+        System.out.println("Length: " + length);
+    }
+}
---
+++ src/doublyLinkedList/Main.java
@@ -0,0 +1,8 @@
+package doublyLinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        DoublyLinkedList newList = new DoublyLinkedList(7);
+        newList.getHead();
+    }
+}
