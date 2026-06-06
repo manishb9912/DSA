012998d59ae7de1f0f25b0008571727e004dca03
manish batgiri <manishbatgiri9912@gmail.com>
2026-06-06 22:39:01 +0530

    added selection sort

---
+++ src/algorithms/sorts/selectionsort/SelectionSort.java
@@ -0,0 +1,27 @@
+package algorithms.sorts.selectionsort;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void selectionSort(int[] array) {
+        for(int i=0;i<array.length;i++) {
+            int minIndex = i;
+            for(int j=i+1;j<array.length;j++) {
+                if(array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            if(i!=minIndex) {
+                int temp = array[i];
+                array[i] = array[minIndex];
+                array[minIndex] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] myArray = {1,4,8,2,5,7};
+        selectionSort(myArray);
+        System.out.println(Arrays.toString(myArray));
+    }
+}
