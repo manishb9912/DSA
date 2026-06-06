ec2468ec5b7fedd65f8f01e2f8bc047a4d21f361
manish batgiri <manishbatgiri9912@gmail.com>
2026-06-06 22:53:51 +0530

    added insertion sort

---
+++ src/algorithms/sorts/insertionsort/InsertionSort.java
@@ -0,0 +1,24 @@
+package algorithms.sorts.insertionsort;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+
+    public static void insertionSort(int[] array) {
+        for(int i=1;i<array.length;i++) {
+            int temp = array[i];
+            int j = i - 1;
+            while(j > -1 && temp < array[j]) {
+                array[j+1] = array[j];
+                array[j] = temp;
+                j--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] myArray = {1,4,7,9,2,5,8};
+        insertionSort(myArray);
+        System.out.println(Arrays.toString(myArray));
+    }
+}
