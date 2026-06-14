ecc7c7bebc5a000b8d819fb9ddaf40a4f9b3fad8
manish batgiri <manishbatgiri9912@gmail.com>
2026-06-14 18:01:09 +0530

    added quick sort

---
+++ src/algorithms/sorts/quicksort/QuickSort.java
@@ -0,0 +1,29 @@
+package algorithms.sorts.quicksort;
+
+import java.util.Arrays;
+
+public class QuickSort {
+    public static void swap(int[] array, int firstIndex, int secondIndex) {
+        int temp = array[firstIndex];
+        array[firstIndex] = array[secondIndex];
+        array[secondIndex] = temp;
+    }
+    public static int pivot(int[] array, int pivotIndex, int endIndex) {
+        int swapIndex = pivotIndex;
+        for(int i = pivotIndex + 1;i<endIndex;i++) {
+            if(array[i] < array[pivotIndex]) {
+                swapIndex++;
+                swap(array, swapIndex, i);
+            }
+        }
+        swap(array, pivotIndex, swapIndex);
+        return swapIndex;
+    }
+    public static void main(String[] args) {
+        int[] myArray = {4,6,1,7,3,2,5};
+
+        int returnedIndex = pivot(myArray, 0, 6);
+        System.out.println("The returned index: " + returnedIndex);
+        System.out.println("Pivot array: " + Arrays.toString(myArray));
+    }
+}
