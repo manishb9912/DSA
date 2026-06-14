11a900dd552b8206c0063e92b3d14b2acd0fa00c
manish batgiri <manishbatgiri9912@gmail.com>
2026-06-14 23:35:07 +0530

    completed quick sort

--- src/algorithms/sorts/quicksort/QuickSort.java
+++ src/algorithms/sorts/quicksort/QuickSort.java
@@ -10,7 +10,7 @@ public class QuickSort {
     }
     public static int pivot(int[] array, int pivotIndex, int endIndex) {
         int swapIndex = pivotIndex;
-        for(int i = pivotIndex + 1;i<endIndex;i++) {
+        for(int i = pivotIndex + 1;i<=endIndex;i++) {
             if(array[i] < array[pivotIndex]) {
                 swapIndex++;
                 swap(array, swapIndex, i);
@@ -19,11 +19,21 @@ public class QuickSort {
         swap(array, pivotIndex, swapIndex);
         return swapIndex;
     }
+    public static void quickSortHelper(int[] array, int left, int right) {
+        if(left < right) {
+            int pivotIndex = pivot(array, left, right);
+            quickSortHelper(array, left, pivotIndex - 1);
+            quickSortHelper(array, pivotIndex + 1, right);
+        }
+    }
+    public static void quickSort(int[] array) {
+        quickSortHelper(array, 0, array.length-1);
+    }
+
     public static void main(String[] args) {
         int[] myArray = {4,6,1,7,3,2,5};
 
-        int returnedIndex = pivot(myArray, 0, 6);
-        System.out.println("The returned index: " + returnedIndex);
+        quickSort(myArray);
         System.out.println("Pivot array: " + Arrays.toString(myArray));
     }
 }
