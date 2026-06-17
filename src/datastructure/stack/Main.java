81594f6e22b36941d1fb5abe0bc3166153617786
manish batgiri <manishbatgiri9912@gmail.com>
2026-06-17 23:48:42 +0530

    added push and pop method to stack

--- src/datastructure/stack/Main.java
+++ src/datastructure/stack/Main.java
@@ -2,9 +2,11 @@ package datastructure.stack;
 
 public class Main {
     public static void main(String[] args) {
-        Stack myStack = new Stack(4);
-        myStack.getTop();
-        myStack.getHeight();
+        Stack myStack = new Stack(7);
+        myStack.push(23);
+        myStack.push(11);
+        myStack.push(15);
+        myStack.pop();
         myStack.printStack();
     }
 }
