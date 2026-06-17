82c6abfab17f7cbcb98295c595420f57572d8482
manish batgiri <manishbatgiri9912@gmail.com>
2026-06-18 00:04:18 +0530

    added queue main file

--- src/datastructure/queue/Main.java
+++ src/datastructure/queue/Main.java
@@ -1,4 +1,8 @@
 package datastructure.queue;
 
 public class Main {
+    public static void main(String[] args) {
+        Queue newQueue = new Queue(11);
+        newQueue.printQueue();
+    }
 }
--- src/datastructure/queue/Queue.java
+++ src/datastructure/queue/Queue.java
@@ -13,9 +13,27 @@ public class Queue {
         }
     }
 
-    public void Queue(int value) {
+    public Queue(int value) {
         Node newNode = new Node(value);
         first = newNode;
         last = newNode;
+        length = 1;
+    }
+
+    public void printQueue() {
+        Node temp = first;
+        while(temp != null) {
+            System.out.println(temp.value);
+            temp=temp.next;
+        }
+    }
+    public void getFirst() {
+        System.out.println("First: " + first.value);
+    }
+    public void getLast() {
+        System.out.println("Last: " + last.value);
+    }
+    public void getLength() {
+        System.out.println("Lenght: " + length);
     }
 }
