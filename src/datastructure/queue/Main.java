c11fde42ec64c04f324498c0afa3c6e0d132c945
manish batgiri <manishbatgiri9912@gmail.com>
2026-06-18 12:03:12 +0530

    Added enqueue and dequeue for queue

--- src/datastructure/queue/Main.java
+++ src/datastructure/queue/Main.java
@@ -3,6 +3,10 @@ package datastructure.queue;
 public class Main {
     public static void main(String[] args) {
         Queue newQueue = new Queue(11);
-        newQueue.printQueue();
+        newQueue.enqueue(22);
+
+        System.out.println(newQueue.dequeue().value);
+        System.out.println(newQueue.dequeue().value);
+        System.out.println(newQueue.dequeue());
     }
 }
--- src/datastructure/queue/Queue.java
+++ src/datastructure/queue/Queue.java
@@ -36,4 +36,31 @@ public class Queue {
     public void getLength() {
         System.out.println("Lenght: " + length);
     }
+
+    public void enqueue(int value) {
+        Node newNode = new Node(value);
+        if(length == 0) {
+            first = newNode;
+            last = newNode;
+        } else {
+            last.next = newNode;
+            newNode.next = null;
+            last = newNode;
+        }
+        length++;
+    }
+
+    public Node dequeue() {
+        if(length == 0) return null;
+        Node temp = first;
+        if(length == 1) {
+            first = null;
+            last = null;
+        } else {
+            first = first.next;
+            temp.next = null;
+        }
+        length--;
+        return temp;
+    }
 }
